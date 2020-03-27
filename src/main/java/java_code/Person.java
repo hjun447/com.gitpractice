9f18a189d621684e77834ac26bc64146cad882ec
Junyoung Heo <junheo448@gmail.com>
2020-03-27 15:26:42 -0400

    Added Constuctor to Person class

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -2,7 +2,8 @@ package java_code;
 
 public class Person {
     private int age;
-    private String firstName;
-    String lastName;
-    
+
+    public Person(int age){
+        this.age = age;
+    }
 }
